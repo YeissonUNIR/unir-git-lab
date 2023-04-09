2c1cc518e2bccb4f124c87f31621e8b486c78faa
Oscar Saavedra <oscarsaavedra06@gmail.com>
2023-04-09 11:15:21 -0500

    Se implementa funcionalidad para la traduccion de palabras en el archivo main

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ Organization: UNIR
 
 import os
 import sys
+import six
+from google.cloud import translate_v2 as translate
 
 DEFAULT_FILENAME = "words.txt"
 DEFAULT_DUPLICATES = False
@@ -20,7 +22,25 @@ def sort_list(items, ascending=True):
 def remove_duplicates_from_list(items):
     return list(set(items))
 
+# Nuevo Oscar Saavedra
+def translate_words(items):
+   
 
+    translate_client = translate.Client()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(items, target_language="en")
+
+    print(u"Text: {}".format(result["input"]))
+    print(u"Translation: {}".format(result["translatedText"]))
+    print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
+
+    return result["translatedText"]
+    
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
     remove_duplicates = DEFAULT_DUPLICATES
@@ -45,5 +65,5 @@ if __name__ == "__main__":
 
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
-
-    print(sort_list(word_list))
+        word_list_translated= translate_words(word_list)
+    print(sort_list(word_list_translated))
